21cb506a770bab60d64a502d1a2ec7ac3e02273a
MateiBabaua <mateibabaua@gmail.com>
2023-12-04 00:42:42 +0200

    Update clevertech_scraper.py

--- sites/clevertech_scraper.py
+++ sites/clevertech_scraper.py
@@ -12,7 +12,7 @@ from __utils import (
 def get_id():
 
     soup = GetStaticSoup('https://clevertech.biz/jobs')
-    script_id = soup.find_all('script', attrs = {'src': '/_next/static/GP71h1Ih1nzvHYndylkaB/_ssgManifest.js'})
+    script_id = soup.find_all('script', attrs = {'src': '/_next/static/mLGCzR2MvQ2raqvmFxc3w/_ssgManifest.js'})
     script_key = str(script_id).split('/')[3]
 
     return script_key
