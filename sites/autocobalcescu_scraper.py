a08c0224fbb81e118ec6007e64e4c5d43270de4f
MateiBabaua <mateibabaua@gmail.com>
2023-12-18 16:10:11 +0200

    Create autocobalcescu_scraper.py

---
+++ sites/autocobalcescu_scraper.py
@@ -0,0 +1,49 @@
+# Company ---> autocobalcescu
+# Link ------> https://www.autocobalcescu.ro/hr
+
+from __utils import (
+    GetStaticSoup,
+    get_county,
+    get_job_type,
+    Item,
+    UpdateAPI,
+)
+
+
+def scraper():
+
+    # scrape data from AutoCobalcescu scraper.
+
+    soup = GetStaticSoup("https://www.autocobalcescu.ro/hr")
+    job_list = []
+    
+    for job in soup.find_all('div', class_= 'col-12'):
+
+        # get jobs items from response
+        job_list.append(Item(
+            job_title = job.find('h6', class_= 'card-title').text.strip(),
+            job_link = job.find('div', class_= 'card-body').find('a')['href'],
+            company = 'AutoCobalcescu',
+            country = 'Romania',
+            county = '',
+            city = '',
+            remote='',
+        ).to_dict())
+
+    return job_list
+
+
+def main():
+
+    company_name = "AutoCobalcescu"
+    logo_link = "https://www.autocobalcescu.ro/images/logo.png"
+
+    jobs = scraper()
+
+    # uncomment if your scraper done
+    UpdateAPI().update_jobs(company_name, jobs)
+    UpdateAPI().update_logo(company_name, logo_link)
+
+
+if __name__ == '__main__':
+    main()
