8beb9786f925d615532770455ad0f44d322c3655
MateiBabaua <mateibabaua@gmail.com>
2023-12-18 16:21:50 +0200

    Create tehnoglobal_scraper.py

---
+++ sites/tehnoglobal_scraper.py
@@ -0,0 +1,49 @@
+# Company ---> tehnoglobal
+# Link ------> https://www.tehnoglobal.ro/cariere/posturi-vacante
+
+from __utils import (
+    GetStaticSoup,
+    get_county,
+    get_job_type,
+    Item,
+    UpdateAPI,
+)
+
+
+def scraper():
+
+    # scrape data from tehnoglobal scraper.
+
+    soup = GetStaticSoup("https://www.tehnoglobal.ro/cariere/posturi-vacante")
+    job_list = []
+    
+    for job in soup.find_all('div', class_ = 'item-job'):
+
+        # get jobs items from response
+        job_list.append(Item(
+            job_title = job.find('h5').text.strip(),
+            job_link = job.find('h5').find('a')['href'],
+            company = 'TehnoGlobal',
+            country = 'Romania',
+            county = get_county('Cluj-Napoca'),
+            city = 'Cluj',
+            remote = '',
+        ).to_dict())
+
+    return job_list
+
+
+def main():
+
+    company_name = "TehnoGlobal"
+    logo_link = "https://www.tehnoglobal.ro/style/images/logo.svg"
+
+    jobs = scraper()
+
+    # uncomment if your scraper done
+    UpdateAPI().update_jobs(company_name, jobs)
+    UpdateAPI().update_logo(company_name, logo_link)
+
+
+if __name__ == '__main__':
+    main()
