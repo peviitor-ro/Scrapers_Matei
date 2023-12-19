e4bd6abf9c89effc38567d75520547f91067526b
MateiBabaua <mateibabaua@gmail.com>
2023-12-19 02:56:20 +0200

    Create saintgobain_scraper.py

---
+++ sites/saintgobain_scraper.py
@@ -0,0 +1,49 @@
+# Company ---> saintgobain
+# Link ------> https://joinus.saint-gobain.com/ro?f%5B0%5D=tara%3Aro
+
+from __utils import (
+    GetStaticSoup,
+    get_county,
+    get_job_type,
+    Item,
+    UpdateAPI,
+)
+
+
+def scraper():
+
+    # scrape data from saintgobain scraper.
+
+    soup = GetStaticSoup("https://joinus.saint-gobain.com/ro?f%5B0%5D=tara%3Aro")
+    job_list = []
+    
+    for job in soup.find_all('div', class_ = 'offer-card-body'):
+
+        # get jobs items from response
+        job_list.append(Item(
+            job_title = job.find('span').text.strip(),
+            job_link = 'https://joinus.saint-gobain.com' + job.find('a')['href'],
+            company = 'SaintGobain',
+            country = 'Romania',
+            county = job.find('div', class_ = 'field__item').text.split()[-1],
+            city = '',
+            remote = '',
+        ).to_dict())
+
+    return job_list
+
+
+def main():
+
+    company_name = "SaintGobain"
+    logo_link = "https://joinus.saint-gobain.com/sites/joinus.saint-gobain.com/files/logo.png"
+
+    jobs = scraper()
+
+    # uncomment if your scraper done
+    UpdateAPI().update_jobs(company_name, jobs)
+    UpdateAPI().update_logo(company_name, logo_link)
+
+
+if __name__ == '__main__':
+    main()
