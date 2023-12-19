23399d84df1cfed581666eb5081c662a1d4ac065
MateiBabaua <mateibabaua@gmail.com>
2023-12-19 02:01:28 +0200

    Create maxbet_scraper.py

---
+++ sites/maxbet_scraper.py
@@ -0,0 +1,52 @@
+#
+# Company ---> maxbet
+# Link ------> https://maxbetgroup.ro/joburi
+
+from __utils import (
+    GetStaticSoup,
+    get_county,
+    get_job_type,
+    Item,
+    UpdateAPI,
+)
+
+
+def scraper():
+
+    # scrape data from maxbet scraper.
+
+    soup = GetStaticSoup("https://maxbetgroup.ro/joburi")
+    job_list = []
+    
+    for job in soup.find_all('div', class_ = 'col-xl-6 col-lg-6 col-md-6 mb-3'):
+
+        oras = str(job.find('div', class_ = 'location').text.strip())
+
+        # get jobs items from response
+        job_list.append(Item(
+            job_title = job.find('div', class_ = 'job-name').text.strip(),
+            job_link = job.find('a')['href'],
+            company = 'MaxBet',
+            country = 'Romania',
+            county = get_county(oras),
+            city = oras,
+            remote='',
+        ).to_dict())
+
+    return job_list
+
+
+def main():
+
+    company_name = "MaxBet"
+    logo_link = "https://maxbetgroup.ro/assets/app/images/maxbet-logo.png"
+
+    jobs = scraper()
+
+    # uncomment if your scraper done
+    UpdateAPI().update_jobs(company_name, jobs)
+    UpdateAPI().update_logo(company_name, logo_link)
+
+
+if __name__ == '__main__':
+    main()
