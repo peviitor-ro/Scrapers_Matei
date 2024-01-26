f3ad21c0219d04483401edb8f68b11a1f7526a81
MateiBabaua <mateibabaua@gmail.com>
2024-01-26 16:33:41 +0200

    Update req_bs4_shorts.py

--- sites/__utils/req_bs4_shorts.py
+++ sites/__utils/req_bs4_shorts.py
@@ -1,70 +1,47 @@
-#
-#
-#  This file contains the Requests + BS4 shorts;
-#  Avoid DRY code, its not a good practice!!!
-#  Make Python3 better place for code!
-#
-#  Start here!
-#
-#
 import requests
 from bs4 import BeautifulSoup
-#
 from .default_headers import DEFAULT_HEADERS
+import xml.etree.ElementTree as ET
 
 
-# Global Session -> avoid multiple requests
-# ... and all classes can use it in one script
 session = requests.Session()
 
-
-class GetStaticSoup:
-    '''
-    ... This class return soup object from static page!
-    '''
+class GetStaticSoup:     # This class return soup object from static page!
 
     def __new__(cls, link, custom_headers=None):
-
         headers = DEFAULT_HEADERS.copy()
 
-        #  if user have custom headers,
-        #  update the headers
         if custom_headers:
             headers.update(custom_headers)
 
         response = session.get(link, headers=headers)
 
         # return soup object from static page
-        return BeautifulSoup(response.content, 'lxml')
+        return BeautifulSoup(response.text, 'lxml')
 
 
-class GetRequestJson:
-    '''
-    ... This class return JSON object from get requests!
-    '''
+class GetRequestJson:     # This class return JSON object from get requests!
 
     def __new__(cls, link, custom_headers=None):
+
         headers = DEFAULT_HEADERS.copy()
 
-        # Dacă utilizatorul are headere personalizate, actualizează headerele
         if custom_headers:
             headers.update(custom_headers)
 
         response = session.get(link, headers=headers)
 
-        # Parse response to JSON and return ditct oject
+        # Parse response to JSON and return dict object
         try:
             json_response = response.json()
             return json_response
-        except ValueError as e:
-            print(f"Errors. No JSON! Details: {e}")
-            return None
+        except ValueError:
+            return BeautifulSoup(response.text, 'lxml')
+        finally:
+            response.close()
 
 
-class PostRequestJson:
-    '''
-    ... This class return JSON object from post requests!
-    '''
+class PostRequestJson:     # This class return JSON object from post requests!
 
     def __new__(cls, url, custom_headers=None, data_raw=None):
         headers = DEFAULT_HEADERS.copy()
@@ -75,29 +52,21 @@ class PostRequestJson:
 
         response = session.post(url, headers=headers, data=data_raw)
 
-        # Parse response to JSON or to soup object
         try:
-            json_response = response.json()
-            return json_response
-        except ValueError as e:
-            pass
+            return response.json()
+        except ValueError:
+            return BeautifulSoup(response.text, 'lxml')
+        finally:
+            response.close()
 
 
-class GetHtmlSoup:
-    '''
-    ... method if server return html response,
-    after post requests.
-    '''
+class GetHtmlSoup:     # Method if server return html response, after post requests.
 
     def __new__(cls, html_response):
         return BeautifulSoup(html_response, 'lxml')
 
 
-class GetHeadersDict:
-    '''
-    ... method if server return headers response,
-    after session.headers
-    '''
+class GetHeadersDict:     # Method if server return headers response, after session.headers
 
     def __new__(cls, url, custom_headers=None):
         headers = DEFAULT_HEADERS.copy()
@@ -106,6 +75,20 @@ class GetHeadersDict:
         if custom_headers:
             headers.update(custom_headers)
 
-        response = session.get(url, headers=headers).headers
+        response = session.head(url, headers=headers).headers
 
         return response
+
+
+class GetXMLObject:     # this class will return data from XML stored in a list
+
+    def __new__(cls, url, custom_headers=None):
+        headers = DEFAULT_HEADERS.copy()
+
+        # if custom headers
+        if custom_headers:
+            headers.update(custom_headers)
+
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return ET.fromstring(response.text)
