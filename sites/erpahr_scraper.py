49d428150cd0ffdac9b341d6b6151d5ca9c0716e
MateiBabaua <mateibabaua@gmail.com>
2024-02-26 15:28:42 +0200

    Update erpahr_scraper.py

--- sites/erpahr_scraper.py
+++ sites/erpahr_scraper.py
@@ -16,14 +16,16 @@ def scraper():
     job_list = []
     
     for job in soup.find_all('div', class_ = 'col-xs-12 col-md-6 col-lg-4 col-xl-3 mv-20'):
-        city = []
-        location_span = job.find('span', class_='pretitle')
-        if location_span == "ROMANIA":
-            next_sibling = location_span.find_next_sibling('span', class_='pretitle_separator')
-            if next_sibling:
-                locations_text = next_sibling.find_next_sibling('span', class_='pretitle').get_text(strip=True)
-                locations = locations_text.split(', ')
-                city = locations if locations else []
+
+        locations = []
+        location_div = job.find('div', class_='career_location')
+        if location_div:
+            location_span = location_div.find('span', class_='pretitle')
+            if location_span and location_span.text == "ROMANIA":
+                next_sibling = location_span.find_next_sibling('span', class_='pretitle_separator')
+                if next_sibling:
+                    locations_text = next_sibling.find_next_sibling('span', class_='pretitle').get_text(strip=True)
+                    locations = locations_text.split(', ')
 
         # get jobs items from response
         job_list.append(Item(
@@ -32,10 +34,10 @@ def scraper():
             company = 'ErpaHR',
             country = 'Romania',
             county = '',
-            city = city,
+            city = locations,
             remote = '',
         ).to_dict())
-
+    print(job_list)
     return job_list
 
 
@@ -47,8 +49,8 @@ def main():
     jobs = scraper()
 
     # uncomment if your scraper done
-    UpdateAPI().update_jobs(company_name, jobs)
-    UpdateAPI().update_logo(company_name, logo_link)
+    # UpdateAPI().update_jobs(company_name, jobs)
+    # UpdateAPI().update_logo(company_name, logo_link)
 
 
 if __name__ == '__main__':
